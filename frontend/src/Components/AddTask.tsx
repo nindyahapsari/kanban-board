25d26f0c5b070755d9d5070d677f00447fa5c9fb
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-02-06 17:25:43 +0100

    add toast feedbaack

--- frontend/src/Components/AddTask.tsx
+++ frontend/src/Components/AddTask.tsx
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
 
 const AddTask = ({ socket }) => {
-    const [task, setTask] = useState('')
+  const [task, setTask] = useState("");
+  const [toastVisible, setToastVisible] = useState(false);
 
   const handleAddTask = () => {
-    console.log("addTask");
+    try {
+      socket.emit("addTask", { task });
+      setTask("");
 
-    socket.emit("addTask", { task })
-    
+      setToastVisible(true);
 
-    setTask('')
+      setTimeout(() => {
+        setToastVisible(false);
+      }, 3000);
+    } catch (error) {}
   };
 
   return (
     <div className="p-4">
+      {toastVisible && (
+        <div className="bg-green-500 text-white px-4 py-2 rounded">
+          Task added successfully!
+        </div>
+      )}
       <input
         className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
         type="search"
@@ -23,6 +33,7 @@ const AddTask = ({ socket }) => {
         onChange={(e) => setTask(e.target.value)}
       />
       <button
+        disabled={task === ""}
         onClick={handleAddTask}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
         type="button"
